eadb526d3cd743324ada9fe66ba5ce896a087911
Diego Coura <diegogomes.10.9@gmail.com>
2023-06-16 00:45:32 -0300

    feat: created data mock

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "Notícia 1",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+        image: "https://static5.depositphotos.com/1034895/434/v/450/depositphotos_4345406-stock-illustration-newspapers-with-lorem-ipsum-copy.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Notícia 2",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+        image: "https://static5.depositphotos.com/1034895/434/v/450/depositphotos_4345406-stock-illustration-newspapers-with-lorem-ipsum-copy.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Notícia 3",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+        image: "https://static5.depositphotos.com/1034895/434/v/450/depositphotos_4345406-stock-illustration-newspapers-with-lorem-ipsum-copy.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Notícia 4",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of",
+        image: "https://static5.depositphotos.com/1034895/434/v/450/depositphotos_4345406-stock-illustration-newspapers-with-lorem-ipsum-copy.jpg",
+        likes: 2,
+        comments: 4
+    },
+]
