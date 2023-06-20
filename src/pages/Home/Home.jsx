71c0a9a0e8a66d7ec68e472a1ae0ccd4138178aa
Diego Coura <diegogomes.10.9@gmail.com>
2023-06-20 19:18:23 -0300

    TextLimit CardStyle update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,34 @@
+import { useState, useEffect } from "react";
 import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
-import { news } from "../../Datas.js";
+import { getAllNews } from "../../services/newsServices";
 import { HomeBody } from "./HomeStyled";
 
+
 export default function Home() {
+  const [news, setNews] = useState([]);
+
+  async function findAllNews() {
+    const response = await getAllNews();
+    setNews(response.data.results);
+  }
+
+  useEffect(()=>{
+    findAllNews();
+  }, []);
   return (
     <>
       <Navbar />
       <HomeBody>
-        {news.map((item, index) => (
-          <Card key={index} news={item} />
+        {news.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes.length}
+            comments={item.comments.length}
+          />
         ))}
       </HomeBody>
     </>
